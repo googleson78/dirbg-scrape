d634992c9dc9fef021d7067275fb30b26a0081cc
Georgi Lyubenov <godzbanebane@gmail.com>
2018-05-13 20:34:00 +0300

    some cleanup

--- src/DirbgScrape/Internal/ProcessResponse.hs
+++ src/DirbgScrape/Internal/ProcessResponse.hs
@@ -7,13 +7,7 @@ import Control.Lens
 import Network.Wreq
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text.Encoding as T
-import Control.Applicative
 import DirbgScrape.Types
-
-asdf :: IO T.Text
-asdf = do
-    a <- get "https://dir.bg/search?q=lol"
-    pure $ T.decodeUtf8 $ BS.toStrict $ a ^. responseBody
 
 articles :: Selector
 articles = "div" @: [hasClass "text-news", hasClass "list-article"]
@@ -25,13 +19,10 @@ links :: Selector -> Scraper T.Text [T.Text]
 links  = attrs "href"
 
 scrapeArticles :: T.Text -> Maybe [Article]
-scrapeArticles html = (fmap (uncurry Article)) <$> scraped
+scrapeArticles htmltext = (fmap (uncurry Article)) <$> scraped
     where scraped = zip <$> titles' <*> links'
-          titles' = scrapeStringLike html (titles articles)
-          links'  = scrapeStringLike html (links articles) -- $ links articles
+          titles' = scrapeStringLike htmltext (titles articles)
+          links'  = scrapeStringLike htmltext (links articles) -- $ links articles
 
 responseText :: Response BS.ByteString -> T.Text
 responseText = T.decodeUtf8 . BS.toStrict . (^. responseBody)
-
-scrape' :: T.Text -> Maybe [Article]
-scrape' html = fmap (fmap (Article "")) $ scrapeStringLike html (links articles)
