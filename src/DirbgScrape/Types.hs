9c758a2dbd3faab0d96750f62eb345ecc2be39a3
Georgi Lyubenov <godzbanebane@gmail.com>
2018-05-13 20:31:06 +0300

    added json serialization "implementation"

--- src/DirbgScrape/Types.hs
+++ src/DirbgScrape/Types.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE DeriveGeneric #-}
 module DirbgScrape.Types
     (Article(..)
     ) where
 
 import Data.Text
+import Data.Aeson
+import GHC.Generics
 
 data Article = Article {name  :: Text,
                         url   :: Text}
-    deriving (Show, Eq)
+    deriving (Generic, Show, Eq)
+
+instance ToJSON Article where
+
+instance FromJSON Article where
